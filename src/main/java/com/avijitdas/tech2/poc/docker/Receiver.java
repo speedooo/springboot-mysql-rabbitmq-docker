6112657425a2052c13514463365226284b40182e
avijitdas-tech2 <avijit123@gmail.com>
2017-05-11 08:03:12 +0530

    Exception handling updated

--- src/main/java/com/avijitdas/tech2/poc/docker/Receiver.java
+++ src/main/java/com/avijitdas/tech2/poc/docker/Receiver.java
@@ -1,11 +1,7 @@
 package com.avijitdas.tech2.poc.docker;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
@@ -19,13 +15,7 @@ public class Receiver {
 		// JSON from String to Object
 		try {
 			customer = new ObjectMapper().readValue(message, Customer.class);
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
